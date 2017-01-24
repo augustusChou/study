9a9475c583a93394c55b29859d788489a99cc4dc
周广 <zhoug@tututech.net>
2017-01-24 17:16:25 +0800

    feat: 添加jar包扫描 vertx-mvc-example pom文件添加打jar包参数

--- study-core/src/main/java/com/myzghome/core/context/AbstractApplicationContext.java
+++ study-core/src/main/java/com/myzghome/core/context/AbstractApplicationContext.java
@@ -4,9 +4,15 @@ import com.myzghome.core.bean.factory.AbstractBeanFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.util.*;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * 作者：周广
@@ -61,6 +67,51 @@ public abstract class AbstractApplicationContext implements ApplicationContext {
                 }
             }
         }
+    }
+
+    //从jar包读取所有class
+    private static void findAndAddClassesInPackageByJar(URL url, String packagePath,
+                                                        String packageDirName, final boolean recursive, Set<Class<?>> classes) {
+        JarFile jar;
+        try {
+            jar = ((JarURLConnection) url.openConnection()).getJarFile();// 获取jar
+            Enumeration<JarEntry> entries = jar.entries();// 从此jar包 得到一个枚举类
+
+            while (entries.hasMoreElements()) {
+                // 获取jar里的一个实体 可以是目录 和一些jar包里的其他文件 如META-INF等文件
+                JarEntry entry = entries.nextElement();
+                String name = entry.getName();
+                // 如果是以/开头的
+                if (name.charAt(0) == '/') {
+                    name = name.substring(1);// 获取后面的字符串
+                }
+                // 如果前半部分和定义的包名相同
+                if (name.startsWith(packageDirName)) {
+                    int idx = name.lastIndexOf('/');
+                    // 如果以"/"结尾 是一个包
+                    if (idx != -1) {
+                        // 获取包名 把"/"替换成"."
+                        packagePath = name.substring(0, idx).replace('/', '.');
+                    }
+                    // 如果可以迭代下去 并且是一个包
+                    if ((idx != -1) || recursive) {
+                        // 如果是一个.class文件 而且不是目录
+                        if (name.endsWith(".class") && !entry.isDirectory()) {
+                            // 去掉后面的".class" 获取真正的类名
+                            String className = name.substring(packagePath.length() + 1, name.length() - 6);
+                            try {
+                                classes.add(Thread.currentThread().getContextClassLoader().loadClass(packagePath + '.' + className));
+                            } catch (ClassNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     //核心方法
@@ -125,6 +176,8 @@ public abstract class AbstractApplicationContext implements ApplicationContext {
                     String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
                     // 以文件的方式扫描整个包下的文件 并添加到集合中
                     findAndAddClassesInPackageByFile(packagePath, filePath, true, classes);
+                } else if ("jar".equals(protocol)) {
+                    findAndAddClassesInPackageByJar(url, packagePath, packageDirName, true, classes);
                 }
             }
         } catch (IOException e) {
